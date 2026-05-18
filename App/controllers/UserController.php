4965b6cb226d0689eb6d10aefa76c4aea121a415
kotikots <kotigithubb@gmail.com>
2026-05-18 12:17:15 +0800

    feat: implement user registration, login views, and job listing creation/edit functionality with form validation.

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -29,4 +29,64 @@ class UserController {
     public function create() {
         loadView('users/create');
     }
-}
+
+    /**
+     * Store user data
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function store() {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $city = $_POST['city'];
+        $state = $_POST['state'];
+        $password = $_POST['password'];
+        $password_confirmation = $_POST['password_confirmation']; 
+
+        $errors = [];
+
+        // validation
+     if(!Validation::email($email)) {
+        $errors['email'] = 'Please Enter a valid email address';
+     }     
+        if(!Validation::string($name, 2, 50)) {
+        $errors['name'] = 'Name must be 2 and 50 characters';
+        }     
+        if(!Validation::string($password, 6, 50)) {    
+        $errors['password'] = 'Password must be 6 characters';
+        }     
+        if(!Validation::match($password, $password_confirmation)) {
+        $errors['password_confirmation'] = 'Passwords do not match';
+        }     
+        
+        if (!empty($errors)) {
+            loadView('users/create',  [
+                'errors' => $errors,  
+                'user' => [
+                    'name' => $name,
+                    'email' => $email,  
+                    'city' => $city,
+                    'state' => $state,
+                ]
+            ]);
+            exit;
+        }     
+        //check if email exist
+        $params = [ 
+            'email' => $email,
+            
+        ];
+
+        $user = $this->db->query("SELECT * FROM users WHERE email = :email", $params); 
+        if($user) {
+            $errors['email'] = 'That email already exists';
+            loadView('users/create',  [
+                'errors' => $errors 
+            ]);
+            exit;  
+        }
+        
+    }
+
+}    
--- routes.php
+++ routes.php
@@ -13,7 +13,7 @@ $router->delete('/listings/{id}', 'ListingController@destroy');
 $router->get('/auth/register', 'UserController@create');
 $router->get('/auth/login', 'UserController@login');
 
-
+$router->post('/auth/register', 'UserController@store');
 
 
 
